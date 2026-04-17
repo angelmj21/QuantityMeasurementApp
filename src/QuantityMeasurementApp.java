cbd8663ba78291b251d10c99325705398770404a
angelmj21 <aj7762@srmist.edu.in>
2026-04-17 15:43:13 +0530

    UC4: Extended Unit Support

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,80 +1,57 @@
 public class QuantityMeasurementApp {
 
-    // Feet class
-    static class Feet {
-        private final double value;
+    enum LengthUnit {
+        FEET(1.0),
+        INCH(1.0 / 12.0),
+        YARD(3.0),
+        CM(0.0328084); // 1 cm = 0.0328084 feet
 
-        public Feet(double value) {
-            this.value = value;
+        private final double toFeetFactor;
+
+        LengthUnit(double toFeetFactor) {
+            this.toFeetFactor = toFeetFactor;
         }
 
-        public double toInches() {
-            return this.value * 12;
+        public double toFeet(double value) {
+            return value * toFeetFactor;
+        }
+    }
+
+    static class Quantity {
+        private final double value;
+        private final LengthUnit unit;
+
+        public Quantity(double value, LengthUnit unit) {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        private double toFeet() {
+            return unit.toFeet(value);
         }
 
         @Override
         public boolean equals(Object obj) {
             if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
 
-            Feet other = (Feet) obj;
-            return Double.compare(this.value, other.value) == 0;
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+
+            Quantity other = (Quantity) obj;
+
+            return Double.compare(this.toFeet(), other.toFeet()) == 0;
         }
-    }
-
-    // Inch class
-    static class Inch {
-        private final double value;
-
-        public Inch(double value) {
-            this.value = value;
-        }
-
-        public double toInches() {
-            return this.value;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) return true;
-            if (obj == null || getClass() != obj.getClass()) return false;
-
-            Inch other = (Inch) obj;
-            return Double.compare(this.value, other.value) == 0;
-        }
-    }
-
-    // Static method for Feet equality
-    public static boolean compareFeet(double v1, double v2) {
-        Feet f1 = new Feet(v1);
-        Feet f2 = new Feet(v2);
-        return f1.equals(f2);
-    }
-
-    // Static method for Inch equality
-    public static boolean compareInch(double v1, double v2) {
-        Inch i1 = new Inch(v1);
-        Inch i2 = new Inch(v2);
-        return i1.equals(i2);
-    }
-
-    // Cross comparison using conversion
-    public static boolean compareFeetAndInch(double feet, double inch) {
-        Feet f = new Feet(feet);
-        Inch i = new Inch(inch);
-
-        return Double.compare(f.toInches(), i.toInches()) == 0;
     }
 
     public static void main(String[] args) {
 
-        // Feet vs Feet
-        System.out.println("Feet Equal: " + compareFeet(1.0, 1.0));
+        Quantity q1 = new Quantity(1.0, LengthUnit.YARD);
+        Quantity q2 = new Quantity(3.0, LengthUnit.FEET);
 
-        // Inch vs Inch
-        System.out.println("Inch Equal: " + compareInch(12.0, 12.0));
+        Quantity q3 = new Quantity(2.54, LengthUnit.CM);
+        Quantity q4 = new Quantity(1.0, LengthUnit.INCH);
 
-        // Feet vs Inch (conversion)
-        System.out.println("Feet-Inch Equal: " + compareFeetAndInch(1.0, 12.0));
+        System.out.println("Yard vs Feet: " + q1.equals(q2)); // true
+        System.out.println("CM vs Inch: " + q3.equals(q4));    // true
     }
 }
